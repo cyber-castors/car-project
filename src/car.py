024aa83bdd15c0dc59aff4a8ffc39843559b2237
cyber-castors <admin@cybercastors.com>
2020-04-19 16:43:18 -0400

    Added start_braking to execute brake till car stops or is accelerated

--- src/car.py
+++ src/car.py
@@ -1,10 +1,6 @@
 import threading
 import time
 
-def add_car():
-    name = input('Enter car name: ')
-    return name
-
 def menu(cars):
     m = '''\nWelcome to the car Program!
     1. Create Car
@@ -24,12 +20,16 @@ def menu(cars):
             cars[int(choice)].accelerate()
         elif option == '3':
             choice = select_car(cars)
-            cars[int(choice)].brake()
+            cars[int(choice)].start_braking()
         elif option == '4':
             choice = select_car(cars)
             cars[int(choice)].run()
         elif option == '5':
             disp_cars(cars)
+
+def add_car():
+    name = input('Enter car name: ')
+    return name
 
 def disp_cars(cars):
     for i, car in enumerate(cars):
@@ -48,25 +48,32 @@ class Car(object):
         self.dist = 0
         self.speed = 0
         self.accel = 0
-        self.brake_accel = 0.5
+        self.brake_accel = 2
         self.cit = 1
         self.interval = 1
         self.is_running = True
+        self.braking = False
 
         thread = threading.Thread(target=self.run, args=())
         thread.daemon = True
         thread.start()
 
     def accelerate(self):
+        if self.braking == True:
+            self.braking = False
         self.accel += 1
         self.dist = self.dist + (self.speed*self.cit) + (self.accel*self.cit**2/2)
         self.speed = self.speed + (self.accel*self.cit)
         #print('The car is traveling at a speed of {0}m/s and has traveled a distance of {1}m'.format(self.speed, self.dist))
 
+    def start_braking(self):
+        self.braking = True
+
     def brake(self):
         self.accel -= self.brake_accel
         if self.speed <= 0:
             self.accel = 0
+            self.speed = 0
             print('Car has come to a halt.')
         elif self.accel > 0:
             self.dist = self.dist + (self.speed*self.cit) - (self.brake_accel*self.cit**2/2)
@@ -81,6 +88,10 @@ class Car(object):
 
     def run(self):
         while True:
+            if self.braking == True:
+                while self.speed != 0:
+                    self.brake()
+                    time.sleep(self.interval)
             self.dist = self.dist + (self.speed*self.cit) + (self.accel*self.cit**2/2)
             self.speed = self.speed + (self.accel*self.cit)
             #print('The car is traveling at a speed of {0}m/s and has traveled a distance of {1}m'.format(self.speed, self.dist))
@@ -90,6 +101,5 @@ class Car(object):
 def main():
     cars = []
     menu(cars)
-    
 
 main()
